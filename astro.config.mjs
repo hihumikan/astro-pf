d5db8b81b82b0be4242ab74d1964d92b4c104035
hihumikan <a@qqey.net>
2024-07-03 13:55:47 +0900

    chore: Remove tsparticlesAstro component and update package.json dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
       noExternal: ["package-name"]
     }
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind()]
 });
